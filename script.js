f803acfa92b069067a815df41c4269bac79e0621
Paulo190409 <paulo.fidelis@escola.pr.gov.br>
2025-10-15 09:31:00 -0300

    .

--- script.js
+++ script.js
@@ -61,3 +61,23 @@ botaoCardapio.addEventListener("click", () => {
     ? "🍔 Ver Cardápio de Hambúrgueres"
     : "❌ Fechar Cardápio";
 });
+ <script src="script.js"></script>
+</body>
+</html>
+<div id="menu">
+  <div id="menu-bar" onclick="menuOnClick()">
+    <div id="bar1" class="bar"></div>
+    <div id="bar2" class="bar"></div>
+    <div id="bar3" class="bar"></div>
+  </div>
+  <nav class="nav" id="nav">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Blog</a></li>
+    </ul>
+  </nav> 
+</div>
+
+<div class="menu-bg" id="menu-bg"></div>
