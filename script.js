d87ca9385ed85a7e83e23761b013de9e8e208250
Paulo190409 <paulo.fidelis@escola.pr.gov.br>
2025-10-15 09:36:04 -0300

    .

--- script.js
+++ script.js
@@ -61,23 +61,8 @@ botaoCardapio.addEventListener("click", () => {
     ? "🍔 Ver Cardápio de Hambúrgueres"
     : "❌ Fechar Cardápio";
 });
- <script src="script.js"></script>
-</body>
-</html>
-<div id="menu">
-  <div id="menu-bar" onclick="menuOnClick()">
-    <div id="bar1" class="bar"></div>
-    <div id="bar2" class="bar"></div>
-    <div id="bar3" class="bar"></div>
-  </div>
-  <nav class="nav" id="nav">
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#">Blog</a></li>
-    </ul>
-  </nav> 
-</div>
-
-<div class="menu-bg" id="menu-bg"></div>
+ unction menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
